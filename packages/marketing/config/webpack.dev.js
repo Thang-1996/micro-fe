d8e5119571d99e33bc799e84687fa9000785b6b1
NguyenMinhThang <thang.nguyenminh@emea.nttdata.com>
2023-04-13 09:40:45 +0700

    Initial commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,29 @@
+const { merge } = require("webpack-merge"); // merge all webpack config into one file
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
